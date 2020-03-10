17e5fc716ea976f278668a49ead3efbe517b338e
Tomas van Stenis <54800674+BigBoiTom@users.noreply.github.com>
2020-03-10 22:52:09 +0100

    Added like.. everything
    yes everything you heard that right

---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    private bool isFalling = true;
+    private float speed = 0.46f;
+    private void OnTriggerEnter(Collider other)
+    {
+        isFalling = false;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 cubePosition = transform.position;
+        if (isFalling == true)
+        {
+            if (Input.GetKeyDown("left"))
+                {
+                cubePosition.x -= speed;
+                Debug.Log("Left.");
+                }
+            if (Input.GetKeyDown("right"))
+            {
+                cubePosition.x += speed;
+                Debug.Log("Right.");
+            }
+        }
+        transform.position = cubePosition;
+
+      
+    }
+
+    
+}
